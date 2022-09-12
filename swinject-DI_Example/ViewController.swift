36512d48cc402fbad0b86d4eeb694eb42f8872b7
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2022-09-13 01:04:09 +0200

    initial commit

--- swinject-DI_Example/ViewController.swift
+++ swinject-DI_Example/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import Swinject
+import SwinjectStoryboard
+
+protocol ABProtocol {
+    func doSomeThing()
+}
+class AClass:ABProtocol{
+    func doSomeThing() { print("instance From AClass") }
+}
+class BClass:ABProtocol{
+    func doSomeThing() {print("instance From BClass")}
+}
+protocol CProtocol{
+    var logEvent: Int {get set}
+    func doSomeThing()
+}
+class CClass: CProtocol {
+    var logEvent: Int
+    init(logEvent: Int) {
+        self.logEvent = logEvent
+    }
+    func doSomeThing() {print("instance From CClass")}
+}
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let instanceFromAClass = Assembler.sharedAssembler.resolver.resolve(ABProtocol.self,name: "AClass")!
+        let instanceFromBClass = Assembler.sharedAssembler.resolver.resolve(ABProtocol.self,name: "BClass")!
+        let instanceFromCClass = Assembler.sharedAssembler.resolver.resolve(CProtocol.self, argument: 3)!
+
+
+        print(instanceFromAClass.doSomeThing())
+        print(instanceFromBClass.doSomeThing())
+        print(instanceFromCClass.doSomeThing())
+        print(instanceFromCClass.logEvent)
+
+
+        //////////////    //////////////    //////////////    //////////////    //////////////    //////////////    //////////////    //////////////    //////////////    /////////////
+        //SwinjectStoryboard
+        
+        
     }
-
-
 }
 
+class AAssembly: Assembly {
+    func assemble(container: Container){
+        container.register(ABProtocol.self,name: "AClass") { resolver in
+            AClass()
+        }
+        container.register(ABProtocol.self, name: "BClass") { resolver in
+            BClass()
+        }
+    }
+}
+
+
+class CAssembly: Assembly {
+    func assemble(container: Container){
+         container.register(CProtocol.self) { (resolver,logEvent: Int) in
+             CClass(logEvent: logEvent)
+         }
+    }
+}
+
+extension Assembler{
+    static let sharedAssembler: Assembler = {
+        let container = Container()
+        let assembler = Assembler(
+            [
+                AAssembly(),
+                CAssembly()
+            ]
+            ,container:container
+        )
+        return assembler
+    }()
+}
